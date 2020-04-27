b2feb093a8e619ff0ef32b0e6f4d8244e419445b
zhangzhongguo <zhongguo.zhang@outlook.com>
2020-04-27 11:25:15 +0800

    turtle

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
-#test you know
-print("test")
+# coding=utf-8
+import turtle
+import time
 
-print("增加一行")
+turtle.pensize(5)
+turtle.pencolor("yellow")
+turtle.fillcolor("red")
+
+turtle.begin_fill()
+for _ in range(9):
+    turtle.forward(200)
+    turtle.right(160)
+turtle.end_fill()
+time.sleep(2)
+
+turtle.penup()
+turtle.goto(-150, -120)
+turtle.color("violet")
+turtle.write("Done", font=('Arial', 40, 'normal'))
+
+turtle.mainloop()
