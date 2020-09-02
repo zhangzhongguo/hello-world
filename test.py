ddd117fb2a5f3442f422d75067dd544e4a6c937d
zhangzhongguo <zhongguo.zhang@outlook.com>
2020-09-02 12:03:28 +0800

    修改颜色为蓝色

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ import time
 
 turtle.pensize(5)
 turtle.pencolor("yellow")
-turtle.fillcolor("red")
+turtle.fillcolor("blue")
 
 turtle.begin_fill()
 for _ in range(9):
     turtle.forward(200)
     turtle.right(160)
 turtle.end_fill()
-time.sleep(2)
+time.sleep(1)
 
 turtle.penup()
 turtle.goto(-150, -120)
