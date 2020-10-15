57deff1204816a3d6a509e01578d7eaba1674450
zhangzhongguo <zhongguo.zhang@outlook.com>
2020-10-15 19:53:58 +0800

    Revert "保留有用的文件"
    This reverts commit 5d8d879c14db1e7efcb9b1d6c19cfe7c0a4341a4.

---
+++ test.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+import turtle
+import time
+
+turtle.pensize(5)
+turtle.pencolor("yellow")
+turtle.fillcolor("blue")
+
+turtle.begin_fill()
+for _ in range(9):
+    turtle.forward(200)
+    turtle.right(160)
+turtle.end_fill()
+time.sleep(3)
+
+turtle.penup()
+turtle.goto(-150, -120)
+turtle.color("violet")
+turtle.write("Done", font=('Arial', 40, 'normal'))
+
+turtle.mainloop()
